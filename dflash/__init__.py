e6978636a94d2d21e5121b4569b918866f254cb4
Trading120 <trading120@users.noreply.github.com>
2026-04-15 19:08:55 +0800

    Add debug flag and clarify import note in __init__.py

--- dflash/__init__.py
+++ dflash/__init__.py
@@ -1,3 +1,9 @@
 # Personal fork of z-lab/dflash
 # Customized for local experimentation and learning
+#
+# Note: imported extract_context_feature for use in local notebooks
+# See experiments/ directory for usage examples
 from .model import DFlashDraftModel, load_and_process_dataset, sample, extract_context_feature
+
+# Default verbosity for local debugging
+DEBUG = False
